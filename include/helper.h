2be2c917658e8effdf74990512d089e32caa0a2e
FrahHS <62186881+FrahHS@users.noreply.github.com>
2020-05-14 17:29:41 +0200

    Add files via upload

---
+++ include/helper.h
@@ -0,0 +1,52 @@
+/*
+MIT License
+Copyright (c) 2020 FrahHS
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef _SHELPER_
+#define _SHELPER_
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+#ifdef __WIN32__ 
+#define _WIN32 1
+#endif //__WIN32__
+
+#ifdef _WIN32
+#include <windows.h>
+#endif //_WIN32
+
+#pragma warning(disable : 4996) //disable errors for unsafe functions
+
+#define INCORRECT -1
+#define READ GENERIC_READ 
+#define WRITE GENERIC_WRITE 
+#ifdef _WIN32
+
+std::wstring towstring(const std::string& str) {    //standard c++ string to wstring converter
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+    std::wstring wstrTo(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+    return wstrTo;
+}
+
+#endif //_WIN32
+#endif //_SHELPER_
+
+
